334da8bcba9ce0ee3cbffbd7bb0926fd4d68aa1b
the-ivo-robotnic <theivorobotnic@gmail.com>
2019-09-12 07:41:39 -0700

    added new mongodb configs

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb+srv://ftf-prod-t1:ubq4dIhnAZWcsf72@dockskiddb-soa85.mongodb.net/test',
+  database: 'mongodb+srv://ftf-prod-t1:ubq4dIhnAZWcsf72@dockskiddb-soa85.mongodb.net/test?retryWrites=true&w=majority',
   secret: 'Christian77777'
 }
