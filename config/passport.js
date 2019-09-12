767450070068d57c42c11dd4f103ca9ed624c1e7
the-ivo-robotnic <theivorobotnic@gmail.com>
2019-09-12 05:59:14 -0700

    created backend, frontend, and template pages

---
+++ config/passport.js
@@ -0,0 +1,21 @@
+const jwt_strategy = require('passport-jwt').Strategy;
+const extract_jwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const config = require('../config/database');
+
+module.exports = function(passport){
+  let opts = {};
+  // opts.jwtFromRequest = extract_jwt.fromAuthHeaderAsBearerToken();
+  opts.jwtFromRequest = extract_jwt.fromAuthHeaderWithScheme('jwt');
+  opts.secretOrKey = config.secret;
+
+  passport.use(new jwt_strategy(opts, function(jwt_payload, done){
+    console.log("Payload: " + JSON.stringify(jwt_payload));
+
+    User.getUserById(jwt_payload._id, function(e, user){
+      if(e){ return done(e, false); }
+      if(user){ return done(null, user); }
+      else{ return done(null, false); }
+    });
+  }));
+}
---
+++ models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//
+// Website user schema
+//
+const user_schema = mongoose.Schema({
+  name: {
+    type: String
+  },
+
+  email: {
+    type: String,
+    required: true
+  },
+
+  username: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = module.exports = mongoose.model('User', user_schema);
+
+module.exports.getUserById = function(id, callback){
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+  const query = {username: username}
+  User.findOne(query, callback);
+}
+
+module.exports.addUser = function(new_user, callback){
+  bcrypt.genSalt(10, function(e, salt){
+    bcrypt.hash(new_user.password, salt, function(e, hash){
+      if(e){ throw  e; }
+      new_user.password = hash;
+      new_user.save(callback);
+    });
+  });
+}
+
+module.exports.comparePassword = function(candidate_password, hash, callback){
+  bcrypt.compare(candidate_password, hash, function(e, is_match){
+    if(e){ throw e; }
+    callback(null, is_match);
+  });
+}
