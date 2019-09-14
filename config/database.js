80aa2471539c5c36260b37c7a24e43f6d31039df
the-ivo-robotnic <theivorobotnic@gmail.com>
2019-09-14 15:13:44 -0700

    updated config. maybe it will work this time?

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb+srv://ftf-prod-t1:ubq4dIhnAZWcsf72@dockskiddb-soa85.mongodb.net/test?retryWrites=true&w=majority',
+  database: 'mongodb+srv://ivo:Dingus_26@dockskiddb-soa85.mongodb.net/test?retryWrites=true&w=majority',
   secret: 'Christian77777'
 }
